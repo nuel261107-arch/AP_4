d31d4ba66ff1f2c0c583311a76b7498bff6e3975
nuel261107-arch <nuel261107@gmail.com>
2026-02-23 09:57:17 +0700

    Add files via upload

---
+++ pertemuan_2.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    system("cls");
+
+    int a, b;
+
+    //assignment operator
+    a = 5;
+    b = 7;
+
+    //arithmetic operator (+ - * / %)
+    // int tambah = a + b;
+    // int kurang = a - b;
+    // int kali = a * b;
+    // int bagi = a / b;
+    // float bagi2 = float(a) / float(b);
+    // int mod = a % b;
+
+    // cout<< "Hasil tambah: " << tambah << endl;
+    // cout<< "Hasil kurang: " << kurang << endl;
+    // cout<< "Hasil kali: " << kali << endl;
+    // cout<< "Hasil bagi (bulat): " << bagi << endl;
+    // cout<< "Hasil bagi (pecahan): " << bagi2 << endl;
+    // cout<< "Hasil mod: " << mod << endl;
+
+    //irrational operator (++, --)
+    //membandingkan kedua nilai dengan operator ==, >, >=, <, <=, !=, 
+    //jika benar akan memberikan output 1 dan o jika salah.
+    // cout<< (a==b)<< endl;
+    // cout<< (a>b)<< endl;
+    // cout<< (a>=b)<< endl;
+    // cout<< (a<b)<< endl;
+    // cout<< (a<=b)<< endl;
+    // cout<< (a!=b)<< endl;
+
+    //logical operator
+    //and (&&)
+    // cout << (true && true) << endl;
+    // cout << (true && false) << endl;
+    // cout << (false && true) << endl;
+    // cout << (false && false) << endl;
+
+    //or (||)
+    // cout << (true || true) << endl;
+    // cout << (true || false) << endl;
+    // cout << (false || true) << endl;
+    // cout << (false || false) << endl;
+
+    //negasi (!)
+    // cout << !(true) << endl;
+    // cout << !(false) << endl;
+    
+    //bitwise operator (&, |, ^, ~, <<, >>)
+    // cout << (5&7) << endl;
+    // cout << (5|7) << endl;
+    // cout << (5^7) << endl;
+    // cout << (~7) << endl;
+    // cout << (7<<2) << endl;
+    // cout << (7>>2) << endl;
+
+    //shorthand
+    a += 2 //a = a + 2
+    cout << a << endl;
+
+    a -= 2 //a = a - 2
+    cout << a << endl;
+
+    a *= 2 //a = a * 2
+    cout << a << endl;
+
+    a /= 2 //a = a / 2
+    cout << a << endl;
+
+    //increment & decrement
+    //pre increment
+    // cout << a << endl;
+    // cout << ++a << endl;
+    // cout << a << endl;
+
+    //post increment
+    // cout << a << endl;
+    // cout << a++ << endl;
+    // cout << a << endl;
+    
+    //pre decrement
+    // cout << a << endl;
+    // cout << --a << endl;
+    // cout << a << endl;
+
+    //post decrement
+    // cout << a << endl;
+    // cout << a-- << endl;
+    // cout << a << endl;
+
+}
