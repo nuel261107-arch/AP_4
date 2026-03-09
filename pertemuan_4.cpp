5c626c402ec221c91301f309d4fa6a1c8d0d8f76
nuel261107-arch <nuel261107@gmail.com>
2026-03-09 10:26:59 +0700

    Add files via upload

---
+++ pertemuan_4.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    system("cls");
+    // goto
+    // goto a;
+    // c:
+    // cout << "anak ";
+    // goto d;
+    // d:
+    // cout << "ilmu ";
+    // goto e;
+    // b:
+    // cout << "adalah ";
+    // goto c;
+    // a:
+    // cout << "Aku ";
+    // goto b;
+    // f:
+    // cout << "USU.\n";
+    // goto n;
+    // e:
+    // cout << "komputer ";
+    // goto f;
+    // n:
+
+    //Menampilkan angka kelipatan lima 
+    // int i = 40;
+
+    // lipat:
+    // if (i % 5 == 0){
+    //     cout << i << " ";
+    // }
+    // i--;
+
+    // if (i>=5){
+    //     goto lipat;
+    // }
+    // cout << endl;
+
+    //while statement
+    // int i = 1;
+    // cout << "\nini while\n";
+    // while (i<=20)
+    // {
+    //     cout << i << ". Hidup Jokowi" << endl;
+    //     i++;
+    // }
+    
+    //do while statement
+    // int i = 10;
+    // string ulang;
+    // cout << "\nini do while\n";
+    // do
+    // {
+    //     cout << i << ". Hidup Jokowi" << endl;
+    //     cout << "Mau ulang lagi? (ulang/tidak) : "; cin >> ulang;
+        
+    // } while (ulang == "ulang");
+    // cout << endl;
+    
+    //for loop
+    //for (inisialisasi, kondisi, indrement/decrement)
+    // for (int i = 5; i >= 1; --i)
+    // {
+    //     cout<<i<< ".Hidup Ganjar" << endl;
+    // }
+    // cout << endl;
+
+    //atribut length
+    // string kata;
+    // cout << "Masukkan kata: "; cin >> kata;
+    // for (int i = 0; i < kata.length(); i++)
+    // {
+    //     cout << i+1 << ". " << kata[i] << endl;
+    // }
+
+    //nested for
+    // for (int i = 1; i <= 5; i++)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+
+    return 0;
+}
